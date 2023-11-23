31b1736434b9d08905d11801ce7251d4d55355ba
Edward Ha <admin@MacBook-Pro-cua-Admin.local>
2023-11-23 21:03:59 +0700

    Feature match ui start screen

---
+++ src/components/Button.js
@@ -0,0 +1,124 @@
+import React, { useCallback, useMemo } from 'react';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { AppTheme, ButtonType } from '../utils/appConstant';
+import TextView from './TextView';
+import TouchableDebounce from './TouchableDebounce';
+
+const ButtonIcon = ({
+  icon,
+  iconSize,
+  iconStyle,
+  text = '',
+  textStyle,
+  iconColor,
+  containerStyle,
+  onPress,
+  loading = false,
+  loadingColor = AppTheme.colors.white,
+  adjustsFontSizeToFit = false,
+  numberOfLines = null,
+  type = ButtonType.PRIMARY, // primary, secondary, disabled
+  disabled = false,
+  ...rest
+}) => {
+  const { backgroundColor, textColor, borderColor, borderWidth } =
+    useMemo(() => {
+      switch (type) {
+        case ButtonType.DISABLE:
+          return {
+            backgroundColor: AppTheme.colors.neutral_20,
+            textColor: AppTheme.colors.neutral_40,
+          };
+        case ButtonType.CANCEL:
+          return {
+            backgroundColor: AppTheme.colors.white,
+            textColor: AppTheme.colors.neutral_60,
+            borderColor: AppTheme.colors.neutral_60,
+            borderWidth: 1,
+          };
+        case ButtonType.SECONDARY:
+          return {
+            backgroundColor: AppTheme.colors.white,
+            textColor: AppTheme.colors.neutral_60,
+          };
+        case ButtonType.PREVIOUS:
+          return {
+            backgroundColor: AppTheme.colors.white,
+            textColor: AppTheme.colors.primary_1,
+            borderColor: AppTheme.colors.primary_1,
+            borderWidth: 1,
+          };
+        default:
+        case ButtonType.PRIMARY:
+          return {
+            backgroundColor: AppTheme.colors.primary_1,
+            textColor: AppTheme.colors.white,
+          };
+      }
+    }, [type]);
+
+  const borderStyle = useMemo(() => {
+    if (type === 'secondary') {
+      return {
+        borderWidth: 1,
+        borderColor: AppTheme.colors.neutral_60,
+      };
+    }
+    return {};
+  }, [type]);
+
+  const renderLoading = useCallback(() => {
+    if (loading) {
+      return (
+        <ActivityIndicator
+          color={loadingColor}
+          size={'small'}
+          style={styles.indicator}
+        />
+      );
+    }
+    return null;
+  }, [loading, loadingColor]);
+
+  return (
+    <TouchableDebounce
+      disabled={disabled}
+      style={[
+        styles.containerStyle,
+        { backgroundColor, borderColor, borderWidth },
+        disabled && styles.disabledView,
+        borderStyle,
+        containerStyle,
+      ]}
+      onPress={onPress}
+      {...rest}
+    >
+      <TextView
+        numberOfLines={numberOfLines}
+        adjustsFontSizeToFit={adjustsFontSizeToFit}
+        style={[styles.txt1, { color: textColor }, textStyle]}
+      >
+        {text}
+      </TextView>
+      {renderLoading()}
+    </TouchableDebounce>
+  );
+};
+
+export default ButtonIcon;
+
+const styles = StyleSheet.create({
+  disabledView: { opacity: 0.6 },
+  ico1: { marginRight: AppTheme.gapSize.s8 },
+  indicator: { marginLeft: AppTheme.gapSize.s8 },
+  txt1: { fontWeight: '600', fontSize: AppTheme.fontSize.s16 },
+  containerStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: AppTheme.buttonHeight,
+    paddingHorizontal: AppTheme.gapSize.s20,
+    paddingVertical: AppTheme.gapSize.s5,
+    borderRadius: AppTheme.gapSize.s10,
+    justifyContent: 'center',
+  },
+});
--- src/screens/StartScreen/StartScreen.js
+++ src/screens/StartScreen/StartScreen.js
@@ -1,12 +1,113 @@
 import React, { memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import TextView from '../../components/TextView';
+import Button from '../../components/Button';
+import IconStart1 from '../../assets/images/StartScreen/management.png';
+import IconStart2 from '../../assets/images/StartScreen/productivity.png';
+import IconStart3 from '../../assets/images/StartScreen/success.png';
+import { AppTheme } from '../../utils/appConstant';
+import Swiper from 'react-native-swiper';
+import FastImage from 'react-native-fast-image';
 
-const StartScreen = () => {
+const slides = [
+  {
+    id: 1,
+    title: 'Easy task & work management with goal',
+    icon: IconStart1,
+  },
+  {
+    id: 2,
+    title: 'Trach your productivity  & gain insights',
+    icon: IconStart2,
+  },
+  {
+    id: 3,
+    title: 'Boost your productivity now & be successful',
+    icon: IconStart3,
+  },
+];
+
+const renderSlide = slides => {
+  const Icon = slides?.icon;
   return (
-    <View>
-      <Text>hello</Text>
+    <View style={styles.slide} key={slides?.id}>
+      <FastImage
+        source={Icon}
+        resizeMode="contain"
+        style={{ height: '40%', width: 300 }}
+      />
+      <TextView
+        fontSize={AppTheme.fontSize.s34}
+        style={{ fontWeight: 600, textAlign: 'center', maxWidth: '80%' }}
+      >
+        {slides?.title}
+      </TextView>
     </View>
   );
 };
+
+const StartScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Swiper
+        key={slides?.length}
+        autoplay={true}
+        showsButtons={false}
+        showsPagination={true}
+        paginationStyle={styles.pagination}
+        onIndexChanged={() => {}}
+        dotColor={AppTheme.colors.neutral_20}
+        activeDotColor={AppTheme.colors.primary_1}
+      >
+        {slides.map(slide => renderSlide(slide))}
+      </Swiper>
+      <View style={styles.footer}>
+        <Button
+          style={styles.buttonNext}
+          text="Next"
+          textStyle={styles.buttonTextStyle}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  footer: {
+    width: '100%',
+    height: '10%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: -3 },
+    // backgroundColor: 'blue',
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 10,
+  },
+  slide: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  pagination: {
+    bottom: 0,
+  },
+  buttonNext: {
+    width: '90%',
+    height: 50,
+    backgroundColor: '#ff585d',
+    borderRadius: 50,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonTextStyle: {
+    color: 'white',
+  },
+});
 
 export default memo(StartScreen);
