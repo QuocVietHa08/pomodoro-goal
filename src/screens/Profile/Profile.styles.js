8312b76b8eaa2913c8603fc855fc8ad31be1b620
Edward Ha <quocvietha08@gmail.com>
2024-01-22 23:20:10 +0700

    Feature profile screen

--- src/screens/Profile/Profile.styles.js
+++ src/screens/Profile/Profile.styles.js
@@ -3,7 +3,9 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    paddingTop: 60,
+    paddingHorizontal: 15,
+    backgroundColor: '#fcfcfc',
   },
 });
 
