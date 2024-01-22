df32fdbdffa0c87ff00c56cab3bab4cccc972252
Edward Ha <quocvietha08@gmail.com>
2024-01-22 17:52:00 +0700

    Feature all completed task

--- src/screens/AllCompletedTask/AllCompletedTask.js
+++ src/screens/AllCompletedTask/AllCompletedTask.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import { View } from 'react-native';
 import styles from './AllCompletedTask.styles';
 import HeaderWrap from 'src/components/HeaderWrap';
 import { FlatList } from 'react-native-reanimated/mock';
 import { tasks } from '../Statistics/mockData';
 import TaskComp from 'src/components/Task';
+import { Swipeable } from 'react-native-gesture-handler';
+import TrashImage from 'src/assets/images/completedTask/trash.png';
+import FastImage from 'react-native-fast-image';
+import TouchableDebounce from 'src/components/TouchableDebounce';
+import ModalDeleteTask from './ModalDeleteTask';
 
 const TodayTask = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [openModalDelete, setOpenModalDelete] = useState(false);
+  let row = [];
+  let prevOpenedRow;
 
   useEffect(() => {
     if (refreshing) {
@@ -21,6 +29,53 @@ const TodayTask = () => {
     setRefreshing(true);
   };
 
+  const handleOpenModalDelete = id => {
+    console.log('item', id);
+    setOpenModalDelete(true);
+  };
+
+  const handleDeteleItem = () => {
+    setOpenModalDelete(false);
+  };
+
+  const renderItem = ({ item, index }, onClick) => {
+    const closeRow = index => {
+      if (prevOpenedRow && prevOpenedRow !== row[index]) {
+        prevOpenedRow.close();
+      }
+      prevOpenedRow = row[index];
+    };
+
+    const renderRightActions = (progress, dragX, onClick) => {
+      return (
+        <TouchableDebounce onPress={onClick}>
+          <View style={styles.deteleIconContainer}>
+            <View style={styles.deleteIconWrap}>
+              <FastImage source={TrashImage} style={styles.deleteIcon} />
+            </View>
+          </View>
+        </TouchableDebounce>
+      );
+    };
+
+    return (
+      <Swipeable
+        renderRightActions={(progress, dragX) =>
+          renderRightActions(progress, dragX, () => {
+            onClick();
+          })
+        }
+        onSwipeableOpen={() => closeRow(index)}
+        ref={ref => (row[index] = ref)}
+        rightOpenValue={-100}
+      >
+        <View>
+          <TaskComp {...item} />
+        </View>
+      </Swipeable>
+    );
+  };
+
   return (
     <View style={styles.container}>
       <HeaderWrap isBackMode titleBack={'All Completed Task (16)'} />
@@ -28,7 +83,17 @@ const TodayTask = () => {
         refreshing={refreshing}
         onRefresh={onRefresh}
         data={tasks}
-        renderItem={({ item }) => <TaskComp {...item} />}
+        renderItem={({ item, index }) =>
+          renderItem({ item, index }, () => {
+            handleOpenModalDelete(item.title);
+          })
+        }
+        keyExtractor={item => item.title}
+      />
+      <ModalDeleteTask
+        onDelete={handleDeteleItem}
+        open={openModalDelete}
+        setOpen={setOpenModalDelete}
       />
     </View>
   );
--- src/screens/AllCompletedTask/AllCompletedTask.styles.js
+++ src/screens/AllCompletedTask/AllCompletedTask.styles.js
@@ -6,6 +6,28 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     backgroundColor: '#fcfcfc',
   },
+  deteleIconContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+    width: 50,
+    height: 100,
+  },
+  deleteIconWrap: {
+    alignItems: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '80%',
+    backgroundColor: '#fee8eb',
+    borderRadius: 10,
+  },
+
+  deleteIcon: {
+    width: 25,
+    height: 25,
+  },
 });
 
 export default styles;
